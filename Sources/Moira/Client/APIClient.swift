46ca2d8f4c32b93b556c2a03b0dd229a79c921b6
weixi <sunshiwei418@126.com>
2026-01-02 23:17:32 +0800

    feat: client format

--- Sources/Moira/Client/APIClient.swift
+++ Sources/Moira/Client/APIClient.swift
@@ -2,11 +2,6 @@ import Foundation
 
 public protocol APIClient {
     func request(_ request: URLRequest) async throws -> APIResponse
-    func upload(
-        _ request: URLRequest,
-        source: UploadSource
-    ) throws -> RequestTask
-    func download(
-        _ request: URLRequest
-    ) throws -> RequestTask
+    func upload(_ request: URLRequest, source: UploadSource) throws -> RequestTask
+    func download(_ request: URLRequest) throws -> RequestTask
 }
